c3dd7c6537e1bf7146047b148d27ff1b1bcf734d
ZFordDev <zforddev@gmail.com>
2026-06-21 13:01:09 +1000

    snapcraft

---
+++ src/schedplus/__main__.py
@@ -0,0 +1,16 @@
+"""
+main.py
+--------
+Entry point for SchedPlus
+
+This module creates the `Scheduler`, asks it to load persisted tasks,
+and launches the selected UI. UIs interact only with the scheduler API.
+
+Note: No Logic in here, keep it simple!
+"""
+
+from startup.controller import boot
+
+
+if __name__ == "__main__":
+    boot()
